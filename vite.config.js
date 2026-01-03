0a5b9dd5558d41f5b8e304e5805d65416fc9b501
Riya6389 <150110397+Riya6389@users.noreply.github.com>
2026-01-03 22:46:42 +0530

    Update base path for K-fitness project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
+// vite.config.js for k fitness
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // This ensures assets are loaded from mks-dev.online/asset.js
+  base: '/K-fitness/', // ✅ required for subpath route
   plugins: [react()],
 });
